16d14cd7b2298c0120fca8079cd8dc2603c48834
Shin hyukjoo <tmxose@naver.com>
2023-07-26 15:15:54 +0900

    1.00

---
+++ app/src/main/java/com/example/groupcalendar/MyApplication.java
@@ -0,0 +1,14 @@
+package com.example.groupcalendar;
+import android.app.Application;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // 앱 초기화 작업을 수행하는 코드를 추가해주세요.
+        // 예: 로깅 라이브러리 초기화, 앱 설정 초기화 등
+    }
+
+    // 추가적인 메서드를 정의하여 앱의 설정이나 초기화 작업을 수행할 수 있습니다.
+    // 예: 특정 설정 값 반환, 데이터 초기화 등
+}
