b3cf40f1b54fb39a165504a2af80b3edc03dfaa8
Shin hyukjoo <tmxose@naver.com>
2023-08-02 11:35:30 +0900

    1.04 Simple text show

--- app/src/main/java/com/example/groupcalendar/DefaultCalendarActivity.java
+++ app/src/main/java/com/example/groupcalendar/DefaultCalendarActivity.java
@@ -1,6 +1,7 @@
 package com.example.groupcalendar;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,7 +11,11 @@ public class DefaultCalendarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_default_calendar);
 
-        // Add your code here to handle default calendar selection
-        // For example, you can show a list of calendars and allow the user to select one.
+        // 기본 캘린더 선택 페이지의 UI 구현
+        // 해당 부분은 activity_default_calendar.xml 파일을 참조하여 구현하시면 됩니다.
+        // 기본 캘린더 선택 페이지의 UI 요소들을 findViewById를 통해 가져와서 처리합니다.
+        // 예시로 TextView를 추가하여 "기본 캘린더 선택 페이지"라는 텍스트를 표시하는 방법을 보여드리겠습니다.
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("기본 캘린더 선택 페이지");
     }
 }
--- app/src/main/java/com/example/groupcalendar/SettingsActivity.java
+++ app/src/main/java/com/example/groupcalendar/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.example.groupcalendar;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,8 +11,12 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        // Add your code here to handle settings
-        // For example, you can allow the user to customize app settings.
+        // 설정 페이지의 UI 구현
+        // 해당 부분은 activity_settings.xml 파일을 참조하여 구현하시면 됩니다.
+        // 설정 페이지의 UI 요소들을 findViewById를 통해 가져와서 처리합니다.
+        // 예시로 TextView를 추가하여 "설정 페이지"라는 텍스트를 표시하는 방법을 보여드리겠습니다.
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("설정 페이지");
     }
 }
 
