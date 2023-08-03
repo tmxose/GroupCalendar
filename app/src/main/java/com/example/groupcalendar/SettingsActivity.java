c9677863495f948cf150f20f81deb860bdb67deb
Shin hyukjoo <tmxose@naver.com>
2023-08-03 16:21:30 +0900

    1.04 Simple text show

--- app/src/main/java/com/example/groupcalendar/SettingsActivity.java
+++ app/src/main/java/com/example/groupcalendar/SettingsActivity.java
@@ -14,7 +14,6 @@ public class SettingsActivity extends AppCompatActivity {
         // 설정 페이지의 UI 구현
         // 해당 부분은 activity_settings.xml 파일을 참조하여 구현하시면 됩니다.
         // 설정 페이지의 UI 요소들을 findViewById를 통해 가져와서 처리합니다.
-        // 예시로 TextView를 추가하여 "설정 페이지"라는 텍스트를 표시하는 방법을 보여드리겠습니다.
         TextView textView = findViewById(R.id.textView);
         textView.setText("설정 페이지");
     }
